8065f90467274dcb8dde4ffc7b18b2d04c36236d
kul-sudo <ulyanov.kirill@protonmail.com>
2025-03-28 16:17:52 +0300

    optimize

--- src/body.rs
+++ src/body.rs
@@ -6,6 +6,7 @@ use std::{collections::HashMap, num::NonZero, time::Instant};
 pub const BODIES_N: NonZero<usize> = NonZero::new(300).unwrap();
 
 pub const INITIAL_MASS: f64 = 0.05;
+pub const LINE_THICKNESS: f32 = 5.0;
 pub const INITIAL_ABS_SPEED: f64 = 0.9;
 
 pub const RADIUS_MULTIPLIER: f64 = 5.0;
@@ -66,18 +67,18 @@ impl Body {
             let mut deepest_connection_depth = f64::MIN;
             let mut deepest_connection_pair: Option<[BodyID; 2]> = None;
 
-            for (lhs_body_id, lhs_body) in bodies.iter() {
-                for (rhs_body_id, rhs_body) in bodies.iter() {
-                    if lhs_body_id == rhs_body_id {
-                        continue;
-                    }
+            let keys = bodies.keys().collect::<Vec<_>>();
+            for (lhs_index, lhs_body_id) in keys.iter().enumerate() {
+                let lhs_body = bodies.get(lhs_body_id).unwrap();
+                for rhs_body_id in &keys[lhs_index + 1..] {
+                    let rhs_body = bodies.get(rhs_body_id).unwrap();
 
                     let depth =
                         lhs_body.radius + rhs_body.radius - (lhs_body.pos - rhs_body.pos).abs();
 
                     if depth >= 0.0 && depth > deepest_connection_depth {
                         deepest_connection_depth = depth;
-                        deepest_connection_pair = Some([*lhs_body_id, *rhs_body_id]);
+                        deepest_connection_pair = Some([**lhs_body_id, **rhs_body_id]);
                     }
                 }
             }
@@ -99,16 +100,16 @@ impl Body {
 
         let mut impossible_positions = false;
 
-        for (lhs_body_id, lhs_body) in bodies.iter() {
-            for (rhs_body_id, rhs_body) in bodies.iter() {
-                if lhs_body_id == rhs_body_id {
-                    continue;
-                }
+        let keys = bodies.keys().collect::<Vec<_>>();
+        for (lhs_index, lhs_body_id) in keys.iter().enumerate() {
+            let lhs_body = bodies.get(lhs_body_id).unwrap();
+            for rhs_body_id in &keys[lhs_index + 1..] {
+                let rhs_body = bodies.get(rhs_body_id).unwrap();
 
-                let dspeed = lhs_body.speed - rhs_body.speed;
-                let a = dspeed.abs().powi(2);
+                if lhs_body.speed != rhs_body.speed {
+                    let dspeed = lhs_body.speed - rhs_body.speed;
+                    let a = dspeed.abs().powi(2);
 
-                if a != 0.0 {
                     let dpos = lhs_body.pos - rhs_body.pos;
 
                     let r = lhs_body.radius + rhs_body.radius;
@@ -125,7 +126,7 @@ impl Body {
 
                             if t_min < earliest_collision_time {
                                 earliest_collision_time = t_min;
-                                earliest_collision_pair = Some([*lhs_body_id, *rhs_body_id]);
+                                earliest_collision_pair = Some([**lhs_body_id, **rhs_body_id]);
                             }
                         } else {
                             impossible_positions = true;
@@ -148,6 +149,8 @@ impl Body {
 
                 match earliest_collision {
                     Some((time, pair)) => {
+                        // Only handles one collisions at a time, because the chance of multiple
+                        // collisions occurring at the same moment is extremely low
                         for body_id in pair {
                             let body = bodies.get(&body_id).unwrap();
                             let collision_pos = body.pos + body.speed * time;
@@ -156,7 +159,7 @@ impl Body {
                                 body.pos.im() as f32,
                                 collision_pos.re() as f32,
                                 collision_pos.im() as f32,
-                                5.0,
+                                LINE_THICKNESS,
                                 RED,
                             );
                         }
@@ -198,76 +201,4 @@ impl Body {
 
         false
     }
-
-    //pub fn get_collisions(bodies: &mut HashMap<BodyID, Body>) -> [Option<(f64, [BodyID; 2])>; 2] {
-    //    let mut earliest_collision_time = f64::INFINITY;
-    //    let mut earliest_collision_pair: Option<[BodyID; 2]> = None;
-    //
-    //    let mut last_separation_time = f64::NEG_INFINITY;
-    //    let mut last_separation_pair: Option<[BodyID; 2]> = None;
-    //
-    //    for (lhs_body_id, lhs_body) in bodies.iter() {
-    //        for (rhs_body_id, rhs_body) in bodies.iter() {
-    //            if lhs_body_id == rhs_body_id {
-    //                continue;
-    //            }
-    //
-    //            let dspeed = lhs_body.speed - rhs_body.speed;
-    //            let a = dspeed.abs().powi(2);
-    //
-    //            if a != 0.0 {
-    //                let dpos = lhs_body.pos - rhs_body.pos;
-    //
-    //                let r = lhs_body.radius + rhs_body.radius;
-    //
-    //                let b = (dpos.re() * dspeed.re() + dpos.im() * dspeed.im()) * 2.0;
-    //                let c = dpos.abs().powi(2) - r.powi(2);
-    //                let d = b.powi(2) - 4.0 * a * c;
-    //
-    //                let d_sqrt = d.sqrt();
-    //                if !d_sqrt.is_nan() {
-    //                    let t_max = (-b + d_sqrt) / (2.0 * a);
-    //                    if t_max >= 0.0 {
-    //                        let t_min = -(b + d_sqrt) / (2.0 * a);
-    //                        if t_min < earliest_collision_time {
-    //                            earliest_collision_time = t_min;
-    //                            earliest_collision_pair = Some([*lhs_body_id, *rhs_body_id]);
-    //                        } else if t_max > last_separation_time {
-    //                            last_separation_time = t_max;
-    //                            last_separation_pair = Some([*lhs_body_id, *rhs_body_id]);
-    //                        }
-    //                    }
-    //                }
-    //            }
-    //        }
-    //    }
-    //
-    //    [
-    //        earliest_collision_pair.map(|pair| (earliest_collision_time, pair)),
-    //        last_separation_pair.map(|pair| (last_separation_time, pair)),
-    //    ]
-    //}
-    //
-    //pub fn update_bodies(lambda: f64, bodies: &mut HashMap<BodyID, Body>) {
-    //    let collision = Self::get_collisions(lambda, bodies);
-    //    match collision[0] {
-    //        Some((time, pair)) => {
-    //            for body in bodies.values_mut() {
-    //                body.pos += body.speed * time;
-    //            }
-    //
-    //            Self::connect(pair, bodies);
-    //            Self::connect_all(bodies);
-    //
-    //            if time < lambda {
-    //                Self::update_bodies(lambda - time, bodies)
-    //            }
-    //        }
-    //        None => {
-    //            for body in bodies.values_mut() {
-    //                body.pos += body.speed * lambda;
-    //            }
-    //        }
-    //    }
-    //}
 }
